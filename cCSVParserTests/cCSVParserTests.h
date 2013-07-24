6157f9995fc9560fea42eb418065eb21c960475b
Jan Weiß <jan@geheimwerk.de>
2013-07-24 13:53:35 +0200

    Adding unit testing infrastructure.

---
+++ cCSVParserTests/cCSVParserTests.h
@@ -0,0 +1,13 @@
+//
+//  cCSVParserTests.h
+//  cCSVParserTests
+//
+//  Created by Jan on 24.07.13.
+//  Copyright (c) 2013 Jan Weiß. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface cCSVParserTests : SenTestCase
+
+@end
---
+++ cCSVParserTests/cCSVParserTests.m
@@ -0,0 +1,34 @@
+//
+//  cCSVParserTests.m
+//  cCSVParserTests
+//
+//  Created by Jan on 24.07.13.
+//  Copyright (c) 2013 Jan Weiß. All rights reserved.
+//
+
+#import "cCSVParserTests.h"
+
+#import "parseCSV.h"
+
+@implementation cCSVParserTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in cCSVParserTests");
+}
+
+@end
